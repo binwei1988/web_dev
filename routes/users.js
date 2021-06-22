72f44ffb40b59352dab2302a5f77a20f5eeedc74
binwei1988 <binwei1988@gatech.edu>
2021-06-21 23:09:07 -0400

    added authorization

--- routes/users.js
+++ routes/users.js
@@ -12,6 +12,7 @@ router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
+        console.log(user);
         const registeredUser = await User.register(user, password);
         req.login(registeredUser, err => {
             if (err) return next(err);
--- seeds/index.js
+++ seeds/index.js
@@ -21,19 +21,19 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
-        const camp = new Campground({
-            author: '5f5c330c2cd79d538f2c66d9',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
-        })
-        await camp.save();
-    }
+    // for (let i = 0; i < 50; i++) {
+    //     const random1000 = Math.floor(Math.random() * 1000);
+    //     const price = Math.floor(Math.random() * 20) + 10;
+    //     // const camp = new Campground({
+    //     //     author: '5f5c330c2cd79d538f2c66d9',
+    //     //     location: `${cities[random1000].city}, ${cities[random1000].state}`,
+    //     //     title: `${sample(descriptors)} ${sample(places)}`,
+    //     //     image: 'https://source.unsplash.com/collection/483251',
+    //     //     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+    //     //     price
+    //     // })
+    //     await camp.save();
+    // }
 }
 
 seedDB().then(() => {
